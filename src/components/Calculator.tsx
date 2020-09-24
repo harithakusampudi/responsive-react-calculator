7066fba7b08b1d5db11f4183fc41044b50cdee5f
haritha <haritha@jhink.com>
2020-09-24 21:01:41 +1000

    Basic calculations using mathjs

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,20 +1,42 @@
-import React from "react";
-import styles from "./styles";
+import React, { useState } from "react";
 import Display from "./Display";
 import KeyPad from "./KeyPad";
+import Styles from "./styles";
+import * as math from "mathjs";
 
-const { CalculatorLayout, Title, Content, Wrapper } = styles;
+const { CalculatorLayout, Title, Content, Wrapper } = Styles;
 
 interface CalculatorProps {}
 
 const Calculator = (Props: CalculatorProps) => {
+  const [inputExpression, setInputExpression] = useState<string>("0");
+
+  const onKeyPress = (key: string) => {
+    if (key === "c") {
+      setInputExpression("0");
+    } else {
+      if (inputExpression === "0") {
+        setInputExpression(math.evaluate(inputExpression + key));
+      } else {
+        setInputExpression(inputExpression + key);
+      }
+    }
+  };
+
+  const onEvaluateExpression = () => {
+    setInputExpression(math.evaluate(inputExpression));
+  };
+
   return (
     <CalculatorLayout>
       <Wrapper>
-        <Title>Calculator</Title>
+        {/* <Title>Calculator</Title> */}
         <Content>
-          <Display />
-          <KeyPad />
+          <Display inputValue={inputExpression} />
+          <KeyPad
+            onKeyPress={onKeyPress}
+            onEvaluateExpression={onEvaluateExpression}
+          />
         </Content>
       </Wrapper>
     </CalculatorLayout>
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,37 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { Row } from "antd";
-import { ColWrapper } from "./KeyPad";
+import Styles from "./styles";
 
-const DisplayWrapper = styled.div`
-  display: flex;
-  flex: 2;
-  flex-direction: column;
-  text-align: right;
-  line-height: 1rem;
-  overflow: scroll;
-`;
+const { ColWrapper, DisplayWrapper, Input } = Styles;
 
-const InputData = styled.input`
-  flex: 1;
-  color: #f0f0f0;
-  font-family: San Francisco;
-  font-size: 40px;
-  max-width: 100%;
-  background-color: inherit !important;
-  border: none;
-  outline: none;
-  text-align: right !important;
-`;
-
-interface DisplayProps {}
+interface DisplayProps {
+  inputValue: string;
+}
 const Display = (props: DisplayProps) => {
-  const number = 1234;
+  const { inputValue } = props;
   return (
     <DisplayWrapper>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         <ColWrapper span={24}>
-          <InputData type="text" readOnly value={number.toLocaleString()} />
+          <Input type="text" readOnly value={inputValue.toLocaleString()} />
         </ColWrapper>
       </Row>
     </DisplayWrapper>
